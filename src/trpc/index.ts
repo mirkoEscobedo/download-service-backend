1730f712be477aa8baf8e08e27368bed8a394bad
mirkoEscobedo <escobedomirko@gmail.com>
2024-11-07 13:27:40 +0100

    now the server work apparently

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import { mediaRouter } from "./mediaRouter";
 import { router } from "./trpc";
+
+dotenv.config();
 
 export const appRouter = router({
   media: mediaRouter,
