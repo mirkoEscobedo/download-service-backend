b47864020beec38a6e6b3408f35113fa245c053a
mirkoEscobedo <escobedomirko@gmail.com>
2024-11-11 20:00:21 +0100

    added maybe useless image-proxy elegigle

--- src/common/middleware/imageProxy.ts
+++ src/common/middleware/imageProxy.ts
@@ -5,15 +5,38 @@ import pino from "pino";
 const logger = pino({ name: "image-proxy" });
 
 export const imageProxyHandler = async (req: Request, res: Response) => {
+  if (req.method === "OPTIONS") {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    return res.sendStatus(204);
+  }
   const { url } = req.query;
   if (!url || typeof url !== "string") {
     return res.status(400).send("invalid image URL");
   }
 
   try {
-    const response = await axios.get(url, { responseType: "arraybuffer" });
-    res.set("Content-Type", response.headers["content-type"]);
-    res.send(response.data);
+    const response = await axios.get(url, {
+      responseType: "arraybuffer",
+      headers: {
+        "User-Agent": "Mozilla/5.0",
+      },
+    });
+    const imageBuffer = Buffer.from(response.data, "binary");
+    const base64Image = imageBuffer.toString("base64");
+    const mimeType = response.headers["content-type"];
+
+    res.set({
+      "Content-Type": mimeType,
+      "Cache-Control": "public, max-age=31536000",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+    });
+
+    console.log(`data:${mimeType};base64,${base64Image}`);
+    res.send(`data:${mimeType};base64,${base64Image}`);
   } catch (error) {
     logger.error("Error fetching image from URL: ", error);
     res.status(500).send("Error fetching image");
